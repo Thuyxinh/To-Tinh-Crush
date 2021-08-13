ae00a632cf02d4639f5b3d5d72741afb7e18b7b2
Thuyxinh <88866088+Thuyxinh@users.noreply.github.com>
2021-08-13 09:09:26 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,21 +1,16 @@
 const CONFIG = {
-    introTitle: 'Này đầu đất',
-    introDesc: `Em thấy nhớ anh, em
-    Em thấy rất nhớ anh thoi
-    Ngày đêm cứ loay hoay
-    Trong suy nghĩ về anh
-    Anh có nhớ em không?
-    Anh có thấy nhớ em không?`,
-    btnIntro: 'hmmmmm',
-    title: 'anh thoát ra là ăn đấm đấy  😌😌',
-    desc: 'Anh có yêu em nhiều hongggg 🥺🥺',
-    btnYes: 'Yêu nhiều lắm luôn 🥺🥺',
-    btnNo: 'Hong, game là tất cả 😗😗',
-    question:'Vì sao anh lại yêu em 🥺🥺',
-    btnReply: 'Gửi cho em nà 😝😝',
-    reply: 'Vì không yêu em đấm á 😘😘',
-    mess: 'Em biết màaaaaaaaaa 😚😚',
-    messDesc: 'Vậy thì bớt chơi game lại đi:))',
-    btnAccept: 'Dạ. Yêu emmmmmmm 🙆🙆',
+    introTitle: 'Này bạn iu',
+    introDesc: `bạn có nhớ tui khum `,
+    btnIntro: 'có hặc không',
+    title: ' thoát ra là ăn đấm đấy  😌😌',
+    desc: 'p có iu quý tui nhiều hong 🥺🥺',
+    btnYes: 'quý nhiều lắm luôn 🥺🥺',
+    btnNo: 'Hong, yêu thương gì 😗😗',
+    question:'Vì sao lại quý tui 🥺🥺',
+    btnReply: 'Gửi cho Thùy neh 😝😝',
+    reply: 'Vì không quý p đấm á 😘😘',
+    mess: 'T biết màaaaaaaaaa 😚😚',
+    messDesc: 'Vậy thì nhớ nhắc bài nhau nhìu nha:))',
+    btnAccept: 'oki lun 🙆🙆',
     messLink: 'http://fb.com' //messages/t/100014015913631
 }
